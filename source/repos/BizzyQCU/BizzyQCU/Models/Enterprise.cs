b2c7352e94026b687ca4279798c6ea48233cfc96
uykurtadrianlusteros-afk <uy.kurtadrian.lusteros@gmail.com>
2026-04-30 00:35:37 +0800

    I hope this works

---
+++ source/repos/BizzyQCU/BizzyQCU/Models/Enterprise.cs
@@ -0,0 +1,13 @@
+﻿namespace BizzyQCU.Models
+{
+    public class Enterprise
+    {
+        public string EnterpriseName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string Type { get; set; }
+        public string DocumentsPath { get; set; }
+    }
+}
